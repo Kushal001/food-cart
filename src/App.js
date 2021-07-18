6d138f1f6e91b64f6544bba46d40f3cb71dcf4ee
Kushal Karki <kushal121320@gmail.com>
2021-07-18 23:54:12 +0545

    backdrop & model styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import "./App.css";
 
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import Modal from "./Components/UI/Modal";
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(true);
+
+  const cartCloseHandler = () => {
+    setIsCartOpen(false);
+  };
+
   return (
     <Fragment>
+      {isCartOpen && (
+        <Modal onClose={cartCloseHandler}>
+          Hello modal
+        </Modal>
+      )}
       <Header />
       <main className="main">
         <Meals />
